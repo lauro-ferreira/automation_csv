8c0cb861c6e9cf61c8c07510f47042cf25cf7884
Lauro Ferreira <lauroferreira1407@gmail.com>
2025-02-05 16:50:57 -0300

    convert csv to xlsx

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # reads a spreadsheet
 # formats the data
 # and generates a report automatically
+import pandas as pd
+import openpyxl
+
+
+def csv_para_excel(arquivo_csv, arquivo_excel):
+    """Converte um arquivo CSV para Excel"""
+    table = pd.read_csv("statistics-central-government.csv")  # Carrega o arquivo csv
+    table.to_excel(arquivo_excel, index=False)  # Salva como excel
+
+    print(f"Convertido com sucesso:{arquivo_excel}")
+
+
+csv_para_excel("statistics.csv", "relatorio.xlsx")
